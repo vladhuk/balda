7a2f7184588458e0f92a846297e8fc700381d289
vladhuk <vladhookovskiy@gmail.com>
2023-01-30 20:08:08 +0200

    register service worker

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import 'index.css';
+import * as serviceWorkerRegistration from 'service-worker-registration';
 import { App } from 'components/App';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { reportWebVitals } from 'utils/report-web-vitals';
@@ -18,6 +19,11 @@ root.render(
     </ThemeProvider>
   </React.StrictMode>,
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
