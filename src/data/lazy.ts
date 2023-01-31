54312350b8c4612c2685dc468ad70b15acfb9263
vladhuk <vladhookovskiy@gmail.com>
2023-01-31 02:03:00 +0200

    split json import into chunks

--- src/data/lazy.ts
+++ src/data/lazy.ts
@@ -1,10 +1,23 @@
+import { range } from 'lodash';
+
+const CHUNKS_NUMBER = 5;
+
 let nouns: string[] = [];
 
+async function fetchNouns(): Promise<string[]> {
+  const modules = await Promise.all<{ default: string[] }>(
+    range(CHUNKS_NUMBER).map((i) => import(`./nouns-${i}.json`)),
+  );
+  return modules
+    .map((module) => module.default)
+    .reduce((acc, chunk) => acc.concat(chunk), []);
+}
+
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
-import('./nouns.json').then((module) => {
-  nouns = module.default;
+fetchNouns().then((fetchedNouns) => {
+  nouns = fetchedNouns;
 });
 
 export function getNouns(): string[] {
-  return nouns;
+  return nouns.slice();
 }
