779d94d66c6d7b018ff0c6ca182fd0ba45d0f47d
vladhuk <vladhookovskiy@gmail.com>
2023-02-11 21:04:07 +0200

    refactor actionsdialog to remove stoppropagation

--- src/components/Game/components/Actions/components/ActionsDialog.tsx
+++ src/components/Game/components/Actions/components/ActionsDialog.tsx
@@ -1,4 +1,12 @@
-import { Backdrop, Box, Button, Fab, Slide, Zoom } from '@mui/material';
+import {
+  Backdrop,
+  Box,
+  Button,
+  ClickAwayListener,
+  Fab,
+  Slide,
+  Zoom,
+} from '@mui/material';
 import { Tutorial } from 'components/_common/Tutorial/Tutorial';
 import CloseIcon from '@mui/icons-material/Close';
 import FlagIcon from '@mui/icons-material/Flag';
@@ -23,18 +31,17 @@ export const ActionsDialog: FC<Props> = ({
   const [isTutorialOpened, setIsTutorialOpened] = useState(false);
 
   return (
-    <>
-      <Backdrop
-        open={open}
-        onClick={onClose}
-        sx={{
-          zIndex: (theme) => theme.zIndex.drawer,
-          flexDirection: 'column',
-          justifyContent: 'start',
-          pt: 28,
-        }}
-      >
-        <div>
+    <Backdrop
+      open={open}
+      sx={{
+        zIndex: (theme) => theme.zIndex.drawer,
+        flexDirection: 'column',
+        justifyContent: 'start',
+        pt: 28,
+      }}
+    >
+      <ClickAwayListener mouseEvent="onMouseUp" onClickAway={onClose}>
+        <Box mb={9}>
           <Slide in={open} direction="up" container={slideContainerRef.current}>
             <Box mb={3}>
               <Button
@@ -62,37 +69,29 @@ export const ActionsDialog: FC<Props> = ({
             </Box>
           </Slide>
           <Slide in={open} direction="up" container={slideContainerRef.current}>
-            <Box mb={9}>
+            <div>
               <Button
                 size="large"
                 color="secondary"
                 endIcon={<HelpOutlineIcon />}
-                onClick={(event) => {
-                  event.stopPropagation();
-                  setIsTutorialOpened(true);
-                }}
+                onClick={() => setIsTutorialOpened(true)}
                 fullWidth
               >
                 Як грати
               </Button>
-            </Box>
+            </div>
           </Slide>
-        </div>
-        <Zoom in={open}>
-          <Fab
-            ref={slideContainerRef}
-            size="large"
-            color="secondary"
-            onClick={onClose}
-          >
-            <CloseIcon />
-          </Fab>
-        </Zoom>
-      </Backdrop>
-      <Tutorial
-        open={isTutorialOpened}
-        onClose={() => setIsTutorialOpened(false)}
-      />
-    </>
+          <Tutorial
+            open={isTutorialOpened}
+            onClose={() => setIsTutorialOpened(false)}
+          />
+        </Box>
+      </ClickAwayListener>
+      <Zoom in={open}>
+        <Fab ref={slideContainerRef} size="large" color="secondary">
+          <CloseIcon />
+        </Fab>
+      </Zoom>
+    </Backdrop>
   );
 };
