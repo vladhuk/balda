dfff4ef29db63c8687dcce02c1d5cc6418a7939c
vladhuk <vladhookovskiy@gmail.com>
2023-08-06 16:36:13 +0300

    enable skipWaiting and clientsClaim

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
   maximumFileSizeToCacheInBytes: 3145728,
+  skipWaiting: true,
+  clientsClaim: true,
 });
 
 /**
